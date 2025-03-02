d37245363f5cf02a9e5f838a4ac88f14ec695515
FariasNawi <NahueMFarias@Gmail.com>
2025-03-02 18:25:31 -0300

    Add files via upload

---
+++ Guia de Funciones 1.py
@@ -0,0 +1,9 @@
+def valor_absoluto():
+    
+    numero = int(input("Ingrese un numero:"))
+    
+    return abs(numero)
+
+resultado = valor_absoluto()
+
+print(f"El valor absoluto del numero ingresado es: {resultado}")
